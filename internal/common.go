d18813d7e2436cc9e3387052b516d96beb6e1084
thekashifmalik <emailkashifmalik@gmail.com>
2024-02-11 11:44:43 -0800

    Add comment about time format

--- internal/common.go
+++ internal/common.go
@@ -4,4 +4,7 @@ var NAME = "rincr"
 var BACKUPS_DIR = "." + NAME
 var BACKUPS_DIR_PATH = "/" + BACKUPS_DIR
 var LAST_FILE_PATH = BACKUPS_DIR_PATH + "/last"
+
+// TIME_FORMAT is the format used to in the backup folder names. The format needs to be file-name-safe. We try to match
+// the universally parse-able ISO format as close as possible.
 var TIME_FORMAT = "2006-01-02T15-04-05"
