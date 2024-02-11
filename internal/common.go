af95f9b5385d616952fb1529e7be5d45d1f5bc03
thekashifmalik <emailkashifmalik@gmail.com>
2024-02-10 23:50:15 -0800

    Update name in internal code

--- internal/common.go
+++ internal/common.go
@@ -1,6 +1,6 @@
 package internal
 
-var BACKUPS_DIR = ".kbackup"
+var BACKUPS_DIR = ".rincr"
 var BACKUPS_DIR_PATH = "/" + BACKUPS_DIR
 var LAST_FILE_PATH = BACKUPS_DIR_PATH + "/last"
 var TIME_FORMAT = "2006-01-02T15-04-05"
