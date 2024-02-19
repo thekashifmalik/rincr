50033a37708a28436a2e89f96fb28a0dc640fc64
thekashifmalik <emailkashifmalik@gmail.com>
2024-02-18 17:36:05 -0800

    Factor out common code in rsync package

--- internal/rsync/rsync.go
+++ internal/rsync/rsync.go
@@ -9,26 +9,19 @@ import (
 )
 
 func Run(source string, destination string) error {
-	rsyncBinary, err := exec.LookPath("rsync")
-	if err != nil {
-		return fmt.Errorf("Cannot find rsync binary: %w", err)
-	}
-	cmd := exec.Command(rsyncBinary, "-hav", "--exclude", internal.BACKUPS_DIR, source, destination)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Run()
-	if err != nil {
-		return fmt.Errorf("Error running rsync: %w", err)
-	}
-	return nil
+	return run("-hav", "--exclude", internal.BACKUPS_DIR, source, destination)
 }
 
 func RunWithDelete(source string, destination string) error {
-	rsyncBinary, err := exec.LookPath("rsync")
+	return run("-hav", "--delete", "--exclude", internal.BACKUPS_DIR, source, destination)
+}
+
+func run(args ...string) error {
+	binary, err := exec.LookPath("rsync")
 	if err != nil {
 		return fmt.Errorf("Cannot find rsync binary: %w", err)
 	}
-	cmd := exec.Command(rsyncBinary, "-hav", "--delete", "--exclude", internal.BACKUPS_DIR, source, destination)
+	cmd := exec.Command(binary, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
