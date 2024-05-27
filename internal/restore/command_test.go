3546ebb12a4d6fece5589cb1ecae96aa6bcaa5ae
thekashifmalik <emailkashifmalik@gmail.com>
2024-05-26 19:22:03 -0700

    Update tests for restore

--- internal/restore/command_test.go
+++ internal/restore/command_test.go
@@ -8,23 +8,44 @@ import (
 	"github.com/thekashifmalik/rincr/internal/restore"
 )
 
-func TestParse(t *testing.T) {
+func TestParseEmpty(t *testing.T) {
 	_, err := restore.Parse(&args.Parsed{})
 	require.Error(t, err)
-	_, err = restore.Parse(&args.Parsed{
+}
+
+func TestParseOnlyCommand(t *testing.T) {
+	_, err := restore.Parse(&args.Parsed{
 		Params: []string{"restore"},
 	})
 	require.Error(t, err)
-	_, err = restore.Parse(&args.Parsed{
+
+}
+
+func TestParseOnlyRepo(t *testing.T) {
+	_, err := restore.Parse(&args.Parsed{
 		Params: []string{"restore", "repo"},
 	})
 	require.Error(t, err)
-	_, err = restore.Parse(&args.Parsed{
+}
+
+func TestParseOnlyPath(t *testing.T) {
+	_, err := restore.Parse(&args.Parsed{
 		Params: []string{"restore", "repo", "path-to-restore"},
 	})
 	require.Error(t, err)
-	cmd, err := restore.Parse(&args.Parsed{
+}
+
+func TestParseMissingMode(t *testing.T) {
+	_, err := restore.Parse(&args.Parsed{
 		Params: []string{"restore", "repo", "path-to-restore", "destination"},
+	})
+	require.Error(t, err)
+}
+
+func TestParse(t *testing.T) {
+	cmd, err := restore.Parse(&args.Parsed{
+		Params:  []string{"restore", "repo", "path-to-restore", "destination"},
+		Options: []string{"--latest"},
 	})
 	require.NoError(t, err)
 	require.Equal(t, "repo", cmd.Respository)
